6e4b1061808a20aa050b5437dc1884f4471cf9f6
ElizavetaFrolova <liza1827frol@gmail.com>
2025-10-13 12:56:46 +0300

    botTelegram

---
+++ python-bot/bot/database_client.py
@@ -0,0 +1,34 @@
+import sqlite3
+import os
+import json
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def recreate_database() -> None:
+    connection=sqlite3.connect(os.getenv('SQLITE_DATABASE_PATH'))
+    with connection:
+        connection.execute("DROP TABLE IF EXISTS telegram_updates")
+        connection.execute("""
+            CREATE TABLE IF NOT EXISTS telegram_updates 
+            (
+                id INTEGER PRYMARY KEY,
+                payload TEXT NOT NULL
+            )
+        """,)
+    connection.close()
+
+def persist_updates(updates: list) -> None:
+    connection=sqlite3.connect(os.getenv('SQLITE_DATABASE_PATH'))
+    with connection:
+        data=[]
+        for update in updates: 
+            data.append(
+                (json.dumps(update, ensure_ascii=False),)
+            )
+        connection.executemany(
+            "INSERT INTO telegram_updates (payload) VALUES (?)",
+            data,
+        )
+    connection.close()
