04eceffa6400cacdb598a20cd9a0f49878fde37e
ElizavetaFrolova <liza1827frol@gmail.com>
2025-10-18 11:59:50 +0300

    added Dispatcher Bot

---
+++ python-dispatcher-bot/bot/__main__.py
@@ -0,0 +1,15 @@
+import bot.database_client
+import bot.telegram_client
+import time
+from bot.dispatcher import Dispatcher
+from bot.handlers.message_echo import MessageEcho
+from bot.long_polling import start_long_polling
+
+
+if __name__=="__main__":
+    try:
+        dispatcher =Dispatcher()
+        dispatcher.add_handler(MessageEcho())
+        start_long_polling(dispatcher)        
+    except KeyboardInterrupt:
+        print("\nBye!")
