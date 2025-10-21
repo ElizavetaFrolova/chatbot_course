a48318348392827cc570ff802d1ccbb1f4ac6b50
ElizavetaFrolova <liza1827frol@gmail.com>
2025-10-21 09:45:21 +0300

    fix DatabaseHandler to handle all updates universally

--- python-dispatcher-bot/bot/handlers/message_db.py
+++ python-dispatcher-bot/bot/handlers/message_db.py
@@ -8,11 +8,6 @@ class DatabaseHandler(Handler):
         return "text" in message or "photo" in message
 
     def handle(self, update: dict) -> bool:
-        msg = update["message"]
-
-        if "photo" in msg:
-            largest_photo = msg["photo"][-1]
-            msg["photo"] = [largest_photo]
-
         bot.database_client.persist_updates([update])
         return True
+
